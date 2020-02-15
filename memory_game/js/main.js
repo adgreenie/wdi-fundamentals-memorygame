0ee42b6c040da2e3c987eb641278b11e4fc4899e
Adam Green <adgreenie@gmail.com>
2020-02-14 23:47:16 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,24 @@
-console.log("Up and running!");
+const cards = ["queen", "queen", "king", "king"];
 
-let cardOne = "queen";
-let cardTwo = cardOne;
-let cardThree = "king";
-let cardFour = cardThree;
+const cardsInPlay = [];
+
+let cardOne = cards[3];
+
+cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
