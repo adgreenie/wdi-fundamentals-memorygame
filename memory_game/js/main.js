4d92ea4a7e04b1a78d634cc356d77f40966ec570
Adam Green <adgreenie@gmail.com>
2020-02-16 01:30:17 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 const cardsInPlay = [];
 
@@ -7,13 +28,15 @@ function checkForMatch(){
 		console.log("You found a match!");
 	}
 	else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
 
 function flipCard(cardID){
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 	}
